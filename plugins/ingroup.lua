345d7cb7ec1643281615d9b76e9e7bad278e5fcf
Pouya <Pouya12@users.noreply.github.com>
2016-05-10 09:10:33 -0700

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -249,8 +249,7 @@ local lock_video = "no"
 local gp_type = data[tostring(msg.to.id)]['group_type']
         
          local settings = data[tostring(target)]['settings']
-	  local text = "⚙Group settings⚙\n====================\nLock group name : "..settings.lock_name.."\n➖➖➖➖\nLock group photo : "..settings.lock_photo.."\n➖➖➖➖\nLock group tag : "..lock_tag.."\n➖➖➖➖\nLock group persian : "..lock_arabic.."\n➖➖➖➖\nLock group chating : "..lock_chat.."\n➖➖➖➖\nLock group member : "..settings.lock_member.."\n➖➖➖➖\nLock group english  "..lock_eng.."\n➖➖➖➖\n Lock group leave : "..lock_leave.."\n➖➖➖➖\nLock group bad words : "..lock_badw.."\n➖➖➖➖\nLock group ads : "..lock_link.."\n➖➖➖➖\nsticker status : "..lock_sticker.."\n➖➖➖➖\nflood sensitivity : "..NUM_MSG_MAX.."\n➖➖➖➖\nBot protection : "..bots_protection.. "\n➖➖➖➖\n Group Type : "..gp_type.."\n➖➖➖➖\n BotVersion : 3.0.0"..tmp
-  if is_realm(msg) then
+         local text = "⚙ Group settings\n====================\nLock group name : "..settings.lock_name.."\n➖➖➖➖\nLock group photo : "..settings.lock_photo.."\n➖➖➖➖\nLock group tag : "..lock_tag.."\n➖➖➖➖\nLock group persian : "..lock_arabic.."\n➖➖➖➖\nLock group chating : "..lock_chat.."\n➖➖➖➖\nLock group member : "..settings.lock_member.."\n➖➖➖➖\nLock group english "..lock_eng.."\n➖➖➖➖\n Lock group leave : "..lock_leave.."\n➖➖➖➖\nLock group bad words : "..lock_badw.."\n➖➖➖➖\nLock group ads : "..lock_link.."\n➖➖➖➖\nsticker status : "..lock_sticker.."\n➖➖➖➖\nflood sensitivity : "..NUM_MSG_MAX.."\n➖➖➖➖\nBot protection : "..bots_protection.. "\n➖➖➖➖\n Group Type : "..gp_type.."\n➖➖➖➖\n Bot v2"..tmp  if is_realm(msg) then
   return text
   else
   return text

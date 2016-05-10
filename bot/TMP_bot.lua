30ceb6f964fe829d0d608ed3ef1274057c48eeda
Pouya <Pouya12@users.noreply.github.com>
2016-05-10 04:24:48 -0700

    Update TMP_bot.lua

--- bot/TMP_bot.lua
+++ bot/TMP_bot.lua
@@ -266,7 +266,7 @@ function create_config( )
     "server",
     "voice"
     },
-    sudo_users = {100577715},--Sudo users
+    sudo_users = {161246734},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     text1 = [[

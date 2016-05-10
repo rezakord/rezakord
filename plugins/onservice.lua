16310bd874eb369ef843c2ee1d40b0c13273a6f1
Pouya <Pouya12@users.noreply.github.com>
2016-05-10 05:21:11 -0700

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,13 +1,13 @@
 do
 --— Will leave the group if be added
 local function run(msg, matches)
-local bot_id = 173296711--— your bot id
+local bot_id = 204820682--— your bot id
    --— like local bot_id = 1234567
     if matches[1] == 'leave' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
       send_large_msg("chat#id"..msg.to.id, 'block shodi :D bye', ok_cb, false)
-            send_large_msg("chat#id"..msg.to.id, 'Rasti banall ham shodi :DDD Bye :D\n\n bia inam channel:\n@telemanager_ch', ok_cb, false)
+            send_large_msg("chat#id"..msg.to.id, 'فقط برای سودو ها مجاز است 🚫', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false) --kick Bot
       banall_user("user#id"..msg.from.id,ok_cb,false) -- Global ban user
       block_user("user#id"..msg.from.id,ok_cb,false) -- block user

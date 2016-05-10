f5c2e81305e57bdc5def9566acb76bdd9a75952e
Pouya <Pouya12@users.noreply.github.com>
2016-05-10 04:45:20 -0700

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,11 +1,11 @@
 do
 
 function run(msg, matches)
-  return [[TeleManagerPlus
+  return [[VoxTg
   
   Bot Version : 2.2.0
   
-  For more Information send /telemanagerplus to chat ]]
+  For more Information send /vox to chat ]]
 end
 
 return {
